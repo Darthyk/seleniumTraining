7893aa73912a70b139993ea260de3eb35a78e254
Vladislav Sazhin <darthy.first@gmail.com>
2017-03-27 22:12:15 +0300

    Fix Task #14

--- java/src/test/java/ru/stqa/training/selenium/CheckReferencesInNewWindows.java
+++ java/src/test/java/ru/stqa/training/selenium/CheckReferencesInNewWindows.java
@@ -16,7 +16,7 @@ import java.util.Set;
 public class CheckReferencesInNewWindows extends CommonSeleniumTest {
 
     /**
-     * Checks links which should be open in another window
+     * Checks links which should be opened in another window
      */
     @Test
     public void checkReferencesInNewWindows() {
